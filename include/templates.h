6a563d5b8a877b8208577a1a04277c1adb820cae
dev-wangxz <1746582382@qq.com>
2022-06-14 16:26:04 +0800

    Add compile time insertion sort

--- include/templates.h
+++ include/templates.h
@@ -19,7 +19,7 @@ struct add_index<t, index_sequence<Idx...>> {
   using result = index_sequence<Idx..., t>;
 };
 
-template<size_t N, typename = std::enable_if<(N > 1)>>
+template<size_t N, typename = std::enable_if<(N > 0)>>
 struct make_index_sequence {
   using result = typename add_index<N-1, typename make_index_sequence<N-1>::result>::result;
 };
@@ -163,22 +163,187 @@ struct type_format<char *> {
   static constexpr auto fmt_str = make_static_string("%s");
 };
 
+template<>
+struct type_format<char []> {
+  static constexpr auto fmt_str = make_static_string("%s");
+};
+
+template<size_t N>
+struct type_format<char [N]> {
+  static constexpr auto fmt_str = make_static_string("%s");
+};
+
 template<typename T>
 using remove_cvr = std::remove_reference<typename std::remove_cv<T>::type>;
 
-template<typename T, typename... Args>
-struct format_string {
-  static constexpr auto str = type_format<typename remove_cvr<T>::type>::fmt_str + format_string<Args...>::str;
-};
+//template<size_t N, const static_string<N>& DELIMITER, typename T, typename... Args>
+//struct format_string {
+//  static constexpr auto str = type_format<typename remove_cvr<T>::type>::fmt_str + DELIMITER +
+//      format_string<N, DELIMITER, Args...>::str;
+//};
+//
+//template<size_t N, const static_string<N>& DELIMITER, typename T>
+//struct format_string<N, DELIMITER, T> {
+//  static constexpr auto str = type_format<typename remove_cvr<T>::type>::fmt_str;
+//};
 
-template<typename T>
-struct format_string<T> {
-  static constexpr auto str = type_format<typename remove_cvr<T>::type>::fmt_str;
-};
 
-template <typename... Args>
-constexpr decltype(format_string<Args...>::str) get_format_string(Args... args) {
-  return format_string<Args...>::str;
+template <size_t N, typename T>
+constexpr auto get_format_string(const char (&)[N], T) {
+  return type_format<T>::fmt_str;
 }
+
+
+template <size_t N, typename T, typename... Args>
+constexpr auto get_format_string(const char (&DELIMITER)[N], T, Args... args) {
+  return type_format<typename remove_cvr<T>::type>::fmt_str +
+  make_static_string(DELIMITER) +
+  get_format_string(DELIMITER, std::forward<Args>(args)...);
+}
+
+template<size_t ...Is>
+struct IntegerSequence{};
+
+template<typename>
+struct IntegerSequence_Front;
+
+template<size_t Front, size_t ...Is>
+struct IntegerSequence_Front<IntegerSequence<Front, Is...>> {
+  static constexpr auto value = Front;
+};
+
+template<typename>
+struct IntegerSequence_Back;
+
+template<size_t Back>
+struct IntegerSequence_Back<IntegerSequence<Back>> {
+  static constexpr auto value = Back;
+};
+
+template<size_t Front, size_t ...Is>
+struct IntegerSequence_Back<IntegerSequence<Front, Is...>> {
+  static constexpr auto value = IntegerSequence_Back<IntegerSequence<Is...>>::value;
+};
+
+template<typename>
+struct IntegerSequence_GetSequence;
+
+template<size_t ...Is>
+struct IntegerSequence_GetSequence<IntegerSequence<Is...>> {
+  static constexpr auto size = sizeof...(Is);
+  static constexpr size_t values[] = {Is...};
+};
+
+template<size_t, typename>
+struct IntegerSequence_PushFront;
+
+template<size_t Front, size_t ...Is>
+struct IntegerSequence_PushFront<Front, IntegerSequence<Is...>> {
+  using type = IntegerSequence<Front, Is...>;
+};
+
+template<typename>
+struct IntegerSequence_PopFront;
+
+template<size_t Front, size_t ...Is>
+struct IntegerSequence_PopFront<IntegerSequence<Front, Is...>> {
+  using type = IntegerSequence<Is...>;
+};
+
+template<size_t, typename>
+struct IntegerSequence_PushBack;
+
+template<size_t Back, size_t ...Is>
+struct IntegerSequence_PushBack<Back, IntegerSequence<Is...>> {
+  using type = IntegerSequence<Is..., Back>;
+};
+
+template<typename>
+struct IntegerSequence_PopBack;
+
+template<size_t Back>
+struct IntegerSequence_PopBack<IntegerSequence<Back>> {
+  using type = IntegerSequence<>;
+};
+
+template<size_t Front, size_t ...Is>
+struct IntegerSequence_PopBack<IntegerSequence<Front, Is...>> {
+  using type = typename IntegerSequence_PushFront<Front, typename IntegerSequence_PopBack<IntegerSequence<Is...>>::type>::type;
+};
+
+template<typename, typename>
+struct IntegerSequence_Concat;
+
+template<size_t... I1s, size_t... I2s>
+struct IntegerSequence_Concat<IntegerSequence<I1s...>, IntegerSequence<I2s...>> {
+  using type = IntegerSequence<I1s..., I2s...>;
+};
+
+template<size_t, typename>
+struct IntegerSequence_Get;
+
+template<size_t... Is>
+struct IntegerSequence_Get<0, IntegerSequence<Is...>> {
+  static constexpr auto value = IntegerSequence_Front<IntegerSequence<Is...>>::value;
+};
+
+template<size_t Idx, size_t... Is>
+struct IntegerSequence_Get<Idx, IntegerSequence<Is...>> {
+  static constexpr auto value = IntegerSequence_Get<Idx-1,
+  typename IntegerSequence_PopFront<IntegerSequence<Is...>>::type
+  >::value;
+};
+
+template<size_t, size_t, typename>
+struct IntegerSequence_Insert;
+
+template<size_t Element, size_t Front, size_t... Is>
+struct IntegerSequence_Insert<0, Element, IntegerSequence<Front, Is...>> {
+  using type = typename IntegerSequence_PushFront<Element, IntegerSequence<Front, Is...>>::type;
+};
+
+template<size_t Idx, size_t Element, size_t Front, size_t... Is>
+struct IntegerSequence_Insert<Idx, Element, IntegerSequence<Front, Is...>> {
+  using type = typename IntegerSequence_PushFront<Front,
+  typename IntegerSequence_Insert<Idx-1, Element, IntegerSequence<Is...>>::type>::type;
+};
+
+template<size_t Element, typename T, bool CONT = (Element > IntegerSequence_Front<T>::value)>
+struct IntegerSequence_AscendingInsert;
+
+template<size_t Element, size_t Front, size_t... Is>
+struct IntegerSequence_AscendingInsert<Element, IntegerSequence<Front, Is...>, false> {
+  using type = typename IntegerSequence_PushFront<Element, IntegerSequence<Front, Is...>>::type;
+};
+
+template<size_t Element, size_t Front, size_t... Is>
+struct IntegerSequence_AscendingInsert<Element, IntegerSequence<Front, Is...>, true> {
+  using type = typename IntegerSequence_PushFront<Front,
+                                                  typename IntegerSequence_AscendingInsert<
+                                                      Element,
+                                                      IntegerSequence<Is...>,
+                                                      (Element > IntegerSequence_Front<IntegerSequence<Is...>>::value)>::type>::type;
+};
+
+template<size_t Element, size_t Front>
+struct IntegerSequence_AscendingInsert<Element, IntegerSequence<Front>, false> {
+  using type = IntegerSequence<Element, Front>;
+};
+
+template<size_t Element, size_t Front>
+struct IntegerSequence_AscendingInsert<Element, IntegerSequence<Front>, true> {
+  using type = IntegerSequence<Front, Element>;
+};
+
+template<size_t Element, size_t... Is>
+struct IntegerSequence_InsertionSortAscending {
+  using type = typename IntegerSequence_AscendingInsert<Element,
+                                                        typename IntegerSequence_InsertionSortAscending<Is...>::type>::type;
+};
+
+template<size_t Element>
+struct IntegerSequence_InsertionSortAscending<Element> {
+  using type = IntegerSequence<Element>;
+};
 
 }
--- main.cpp
+++ main.cpp
@@ -5,9 +5,33 @@
 constexpr char s1[] = "Hello cpp template!";
 constexpr char s2[] = "Hello again!";
 
+constexpr char DELIMITER[] = " ";
+
+constexpr auto fs = MyTemplates::get_format_string(DELIMITER, 1, 2, 3, 4, 'a', 3.2, 4.f);
+const char *cs = fs.content;
+
+struct TestSt {
+  int a;
+  long b;
+  float c;
+  double d;
+  char e;
+  char s[16];
+
+  static constexpr auto fmt_string = MyTemplates::get_format_string(
+      ",",
+      decltype(TestSt::a){},
+      decltype(TestSt::b){},
+      decltype(TestSt::c){},
+      decltype(TestSt::d){},
+      decltype(TestSt::e){},
+      std::decay<decltype(TestSt::s)>::type{}
+  );
+};
+
 int main(int argc, char **argv) {
-  MyTemplates::static_string<sizeof(s1)-1> ss1(s1);
-  MyTemplates::static_string<sizeof(s2)-1> ss2(s2);
+  MyTemplates::static_string<sizeof(s1) - 1> ss1(s1);
+  MyTemplates::static_string<sizeof(s2) - 1> ss2(s2);
   auto ss3 = ss1 + ss2;
   MyTemplates::static_string<0> s0("");
   auto ss4 = ss3 + s0;
@@ -19,6 +43,57 @@ int main(int argc, char **argv) {
   auto ss6 = MyTemplates::make_static_string("");
   std::cout << ss6.content << std::endl;
 
-  std::cout << MyTemplates::get_format_string(1,2,3,4,'a',3.2,4.f).content << std::endl;
+  std::cout << cs << std::endl;
+
+  std::cout << TestSt::fmt_string.content << std::endl;
+
+  std::cout << MyTemplates::IntegerSequence_Back<MyTemplates::IntegerSequence<5, 2, 6, 9, 1, 3>>::value << std::endl;
+  using SEQUENCE = MyTemplates::IntegerSequence_GetSequence<MyTemplates::IntegerSequence<5, 2, 6, 9, 1, 3>>;
+  for (int i = 0; i < SEQUENCE::size; ++i) {
+    std::cout << SEQUENCE::values[i];
+  }
+  std::cout << std::endl;
+
+  using SEQUENCE2 = MyTemplates::IntegerSequence_GetSequence<
+      MyTemplates::IntegerSequence_PopBack<MyTemplates::IntegerSequence<5, 2, 6, 9, 1, 3>>::type
+  >;
+  for (int i = 0; i < SEQUENCE2::size; ++i) {
+    std::cout << SEQUENCE2::values[i];
+  }
+  std::cout << std::endl;
+
+  using SEQUENCE3 = MyTemplates::IntegerSequence_GetSequence<
+      MyTemplates::IntegerSequence_Concat<
+          MyTemplates::IntegerSequence<1, 2, 3>,
+          MyTemplates::IntegerSequence<4, 5, 6>>::type
+  >;
+  for (int i = 0; i < SEQUENCE3::size; ++i) {
+    std::cout << SEQUENCE3::values[i];
+  }
+  std::cout << std::endl;
+
+  std::cout << MyTemplates::IntegerSequence_Get<2, MyTemplates::IntegerSequence<1, 2, 3>>::value << std::endl;
+
+  using SEQUENCE4 = MyTemplates::IntegerSequence_GetSequence<
+      typename MyTemplates::IntegerSequence_Insert<3, 10,
+                                                   MyTemplates::IntegerSequence<1, 2, 3, 4, 5>>::type>;
+  for (int i = 0; i < SEQUENCE4::size; ++i) {
+    std::cout << SEQUENCE4::values[i];
+  }
+  std::cout << std::endl;
+
+  using SEQUENCE5 = MyTemplates::IntegerSequence_GetSequence<MyTemplates::IntegerSequence_AscendingInsert<3,
+  MyTemplates::IntegerSequence<1,2,3,4,5,6,7>>::type>;
+  for (int i = 0; i < SEQUENCE5::size; ++i) {
+    std::cout << SEQUENCE5::values[i];
+  }
+  std::cout << std::endl;
+
+  using SEQUENCE6 = MyTemplates::IntegerSequence_GetSequence<
+      MyTemplates::IntegerSequence_InsertionSortAscending<9,5,2,6,4,8,7,1,3>::type>;
+  for (int i = 0; i < SEQUENCE6::size; ++i) {
+    std::cout << SEQUENCE6::values[i];
+  }
+  std::cout << std::endl;
   return 0;
 }
